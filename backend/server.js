f530d02576959365be5c9451a8a7f167ecfd9d93
jaideep <jaideepmahala85@gmail.com>
2026-08-01 18:33:59 +0000

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
-import express from express;
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT || 5050;
+
 
 const app = express();
 
+app.get("/home" , (req,res) => res.send("homepage is working sucessfully"));
+app.get("/", (req, res) => res.send("backend is working successfully"));
 
+app.listen(port , () => console.log(`the server is running at : ${port}`));
